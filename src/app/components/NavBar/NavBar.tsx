c9a044f57f568834f80543bdaf530ada055b9fc8
Caleb Stevens <calebjamesstevens219@gmail.com>
2023-06-15 16:29:00 -0700

    bv-1 added menu drawer

--- src/app/components/NavBar/NavBar.tsx
+++ src/app/components/NavBar/NavBar.tsx
@@ -1,14 +1,36 @@
 'use client'
 
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import React from 'react';
 
+import { AppBar, Box, Drawer, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Stack, SwipeableDrawer, Toolbar, Typography } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import DescriptionIcon from '@mui/icons-material/Description';
 
 export default () => {
+    const [navigationMenuOpen, setNavigationMenuOpen] = React.useState(false);
+    const navigationMenuId = React.useId();
 
     return (
-        <AppBar position="sticky">
-            <Toolbar>
-                <Typography variant="body1">Brag Vault</Typography>
+        <AppBar sx={{background: 'white'}} position="sticky">
+            <Toolbar sx={{background: 'none'}}>
+                <Stack alignItems={'center'} width={'100%'} direction={'row'} justifyContent={'space-between'}>
+                    <img src='logo.svg' alt="Brag Vault Logo" height={24}/>
+                    <IconButton onClick={() => setNavigationMenuOpen(true)} aria-controls={navigationMenuId} aria-haspopup={'dialog'} aria-label="Open Navigation Menu" size="medium">
+                        <MenuIcon fontSize="medium" />
+                    </IconButton>
+                    <Drawer sx={{height: '100dvh' }} id={navigationMenuId} anchor='right' open={navigationMenuOpen} onClose={() => setNavigationMenuOpen(false)}>
+                        <Box component={'nav'} sx={{height:'auto', width: '100%'}}>
+                            <List sx={{width: 250}}>
+                                <ListItem disablePadding >
+                                    <ListItemButton >
+                                        <ListItemIcon aria-hidden><DescriptionIcon/></ListItemIcon>
+                                        <ListItemText>Brag Document</ListItemText>
+                                    </ListItemButton>
+                                </ListItem>
+                            </List>                        
+                        </Box>
+                    </Drawer>
+                </Stack>
             </Toolbar>
         </AppBar>
     )
